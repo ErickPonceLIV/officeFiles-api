864db3b079739de512bcfafe5bc4d242c3e2ac5f
ErickPonceLIV <erickponce2093@gmail.com>
2024-10-29 14:12:40 -0600

    "Updated documentSchema in officeFiles model: renamed 'documentSerial' to 'documentId', added comments to 'management' and 'directorate' fields."

--- models/officeFiles.js
+++ models/officeFiles.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 const documentSchema = new mongoose.Schema({
   year: { type: String, required: true }, // Se almacena como String ya que podría contener caracteres adicionales en ciertos formatos
-  management: { type: String, required: true },
-  directorate: { type: String, required: true },
-  documentSerial: { type: String, required: true, unique: true }, // Identificador único para el documento
+  management: { type: String, required: true }, // Se almacena la dirección a la que pertenece el archivo
+  directorate: { type: String, required: true }, // Se almacena el departamento donde esta ubicado el archivo
+  documentId: { type: String, required: true, unique: true }, // Identificador único para el documento
   name: { type: String, required: true },
   preservation: { type: String, required: true }, // Almacenado como String ya que indica la duración de conservación
   status: [{ type: String }], // Almacena un array de strings para los estados del documento
