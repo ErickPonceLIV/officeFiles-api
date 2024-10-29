7d00d585384cac7dce9a9415aa9c784856fa9008
ErickPonceLIV <erickponce2093@gmail.com>
2024-10-29 15:47:46 -0600

    add routes and add createFile route with CRUD

---
+++ routes/fileRoutes.js
@@ -0,0 +1,9 @@
+// importamos express con ESM
+import express from 'express'
+import * as fileController from '../controllers/filesController.js'
+
+// Hago mi constante para manejar las rutas
+const fileRoutes = express.Router()
+
+// Defino las rutas
+fileRoutes.post('/file', fileController.createFile)
